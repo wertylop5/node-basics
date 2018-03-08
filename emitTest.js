0dddc03a7f7096f90e7a1092e6f69b9628901282
Stanley Lin <slin55055@gmail.com>
2018-03-08 14:18:46 -0500

    works on homer

--- emitTest.js
+++ emitTest.js
@@ -18,9 +18,13 @@ hi.on({yo: "5"}, () => {
 	console.log("uhhh");
 });
 
+/*
+Will work on Node 6+
+
 hi.prependListener({yo: "5"}, () => {
 	console.log("cutting the line");
 });
+*/
 
 hi.emit({yo: "5"});
 
